3215b9692ceab6abba07d9b56ad24177a2d6c98e
chih <chihao.thai@gmail.com>
2025-09-14 00:14:42 +0700

    fix bugs

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,15 @@ import Footer from "@/components/Footer";
 import Hero from "@/components/HeroBanner";
 import PromotionBanner from "@/components/PromotionBanner";
 import Rackets from "@/components/Rackets";
-import { db } from "@/lib/firebase";
+// import { db } from "@/lib/firebase";
 import { getTop3Rackets } from "@/lib/getTop3Rackets";
 import { Racket } from "@/types/rackets";
 
-import { collection, getDocs } from "firebase/firestore";
+// import { collection, getDocs } from "firebase/firestore";
 
 export default async function HomePage() {
-  const racketsCol = collection(db, "products");
-  const snapshot = await getDocs(racketsCol);
+  // const racketsCol = collection(db, "products");
+  // const snapshot = await getDocs(racketsCol);
 
   // const rackets: Racket[] = snapshot.docs.map((doc) => {
   //   const data = doc.data();
